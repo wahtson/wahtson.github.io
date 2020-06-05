a122b2eb6f9245f5af1dac6fddc0e39e252e4fa0
Barnaby <barnab.why@gmail.com>
2020-06-05 09:26:21 +0100

    Test

--- script/wiki.js
+++ script/wiki.js
@@ -1,1 +1,17 @@
+require.config({
+    paths : {
+        text: 'script/require/json',
+        async: 'script/require/async',
+        json: 'script/require/json'
+    }
+});
 
+let route = location.hash.substr(1).split('~@~')
+let version = route[1]
+let path
+if(route[0] == 'config.toml') path = 'config'
+console.log(version)
+
+require(['json!./data/'+version+'/'+path+'.json'], (data) => {
+    document.write(data)
+})
